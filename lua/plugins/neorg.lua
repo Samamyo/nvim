91e05797d91c56bf96331b9e2a674c9bacb8247d
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-03-27 15:41:05 +1100

    neorg 8 w/ modueles

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,83 +1,86 @@
 return {
-	"nvim-neorg/neorg",
-	build = ":Neorg sync-parsers",
-	ft = "norg",
-	tag = "v7.0.0",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-neorg/neorg-telescope",
-		{ "pysan3/neorg-templates", dependencies = { "L3MON4D3/LuaSnip" } },
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
 	},
+	{
+		"nvim-neorg/neorg",
+		ft = "norg",
+		dependencies = {
+			{ "luarocks.nvim" },
+		},
 
-	config = function()
-		require("neorg").setup({
-			load = {
-				["core.defaults"] = {}, -- Loads default behaviour
-				["core.concealer"] = {
-					config = {
-						icons = {
-							todo = {
-								urgent = { icon = "" },
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {
+						config = {
+							icons = {
+								todo = {
+									urgent = { icon = "" },
+								},
 							},
 						},
+					}, -- adds pretty icons to your documents
+					["core.completion"] = {
+						config = { engine = "nvim-cmp" },
 					},
-				}, -- adds pretty icons to your documents
-				["core.completion"] = {
-					config = { engine = "nvim-cmp" },
-				},
-				["core.summary"] = {},
+					["core.summary"] = {},
 
-				["core.integrations.treesitter"] = {},
+					["core.integrations.treesitter"] = {},
 
-				["core.integrations.telescope"] = {},
+					["core.integrations.telescope"] = {},
 
-				["core.esupports.metagen"] = {
-					config = { timezones = "implicit-local", type = "auto" },
-				},
-
-				["core.journal"] = {
-					config = {
-						strategy = "flat",
-						workspace = "notes",
+					["core.esupports.metagen"] = {
+						config = { timezones = "implicit-local", type = "auto" },
 					},
-				},
 
-				["core.dirman"] = {
-					config = {
-						workspaces = {
-							notes = "~/Notes/notes",
-							work = "~/Notes/work",
+					["core.journal"] = {
+						config = {
+							strategy = "flat",
+							workspace = "notes",
 						},
-						default_workspace = "notes",
+					},
+
+					["core.dirman"] = {
+						config = {
+							workspaces = {
+								notes = "~/Notes/notes",
+								work = "~/Notes/work",
+							},
+							default_workspace = "notes",
+						},
+					},
+
+					["core.keybinds"] = {
+						config = {
+							hook = function(keybinds)
+								keybinds.map("norg", "n", "<localleader>r", ":Neorg return<CR>")
+								keybinds.map("norg", "i", "-(", "- ( ) ")
+							end,
+						},
 					},
 				},
-
-				["core.keybinds"] = {
-					config = {
-						hook = function(keybinds)
-							keybinds.map("norg", "n", "<localleader>r", ":Neorg return<CR>")
-							keybinds.map("norg", "i", "-(", "- ( ) ")
-						end,
-					},
-				},
-			},
-		})
-		local neorg_callbacks = require("neorg.core.callbacks")
-
-		neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-			-- Map all the below keybinds only when the "norg" mode is active
-			keybinds.map_event_to_mode("norg", {
-				n = { -- Bind keys in normal mode
-					{ "<C-s>", "core.integrations.telescope.find_linkable" },
-				},
-
-				i = { -- Bind in insert mode
-					{ "<C-l>", "core.integrations.telescope.insert_link" },
-				},
-			}, {
-				silent = true,
-				noremap = true,
 			})
-		end)
-	end,
+			local neorg_callbacks = require("neorg.core.callbacks")
+
+			neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
+				-- Map all the below keybinds only when the "norg" mode is active
+				keybinds.map_event_to_mode("norg", {
+					n = { -- Bind keys in normal mode
+						{ "<C-s>", "core.integrations.telescope.find_linkable" },
+					},
+
+					i = { -- Bind in insert mode
+						{ "<C-l>", "core.integrations.telescope.insert_link" },
+					},
+				}, {
+					silent = true,
+					noremap = true,
+				})
+			end)
+		end,
+	},
 }
