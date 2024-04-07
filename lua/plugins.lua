14b1d7e06c1ab70645171c2843816a6123a337fd
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-04-07 20:00:29 +1000

    actually adding black and bringing back auto-tab

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,5 +23,6 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
+        config = true
 	},
 }
--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"vhyrro/luarocks.nvim",
-		priority = 1000,
+        lazy = true,
 		config = true,
 	},
 	{
