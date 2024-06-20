b3b751b9b2f6da1540d275c5980bc7e9fe51fee5
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-06-20 14:42:40 +1000

    more formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,17 @@
 return {
-    "nvimtools/none-ls.nvim",
-    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    config = function()
-        local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	config = function()
+		local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.black,
-            },
-        })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.clang_format,
+			},
+		})
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
