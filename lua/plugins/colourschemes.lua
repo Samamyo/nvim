0b311f5a7455bf20300c3722947abad17ffbed80
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-06-22 13:06:25 +1000

    giving nightfox transparent background

--- lua/plugins/colourschemes.lua
+++ lua/plugins/colourschemes.lua
@@ -1,14 +1,23 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		name = "kanagawa",
-		priority = 1000,
-		config = function()
-			require("kanagawa").setup({
-				transparent = true, -- do not set background color
-			})
-			vim.cmd.colorscheme("kanagawa")
-		end,
-	},
-	{ "EdenEast/nightfox.nvim" }, -- lazy
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        priority = 1000,
+        config = function()
+            require("kanagawa").setup({
+                transparent = true, -- do not set background color
+            })
+            vim.cmd.colorscheme("kanagawa")
+        end,
+    },
+    {
+        "EdenEast/nightfox.nvim",
+        config = function()
+            require("nightfox").setup({
+                options = {
+                    transparent = true,
+                }
+            })
+        end,
+    },
 }
