9eedc3d383a012f0550328e1ed9d76f4bad49057
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-06-22 13:08:05 +1000

    adding vim-fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,17 +1,22 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    config = function()
-        require('gitsigns').setup {
-            signs = {
-                add          = { text = '│' },
-                change       = { text = '│' },
-                delete       = { text = '_' },
-                topdelete    = { text = '‾' },
-                changedelete = { text = '~' },
-                untracked    = { text = '┆' },
-            },
-            signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-        }
-    end,
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "│" },
+					change = { text = "│" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			})
+		end,
+	},
 }
