56cce538580296cadf15ff633971708d4a72037e
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-06-29 13:21:43 +1000

    no transparency and new themes

--- lua/plugins/colourschemes.lua
+++ lua/plugins/colourschemes.lua
@@ -1,23 +1,30 @@
 return {
-    {
-        "rebelot/kanagawa.nvim",
-        name = "kanagawa",
-        priority = 1000,
-        config = function()
-            require("kanagawa").setup({
-                transparent = true, -- do not set background color
-            })
-            vim.cmd.colorscheme("kanagawa")
-        end,
-    },
-    {
-        "EdenEast/nightfox.nvim",
-        config = function()
-            require("nightfox").setup({
-                options = {
-                    transparent = true,
-                }
-            })
-        end,
-    },
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({
+				-- transparent = true, -- do not set background color
+			})
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+	{
+		"EdenEast/nightfox.nvim",
+		config = function()
+			require("nightfox").setup({
+				options = {
+					-- transparent = true,
+				},
+			})
+		end,
+	},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
 }
