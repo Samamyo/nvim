9ca0f8caf0edaed4af8f2c49300fe1e7e5905cbb
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-06-29 13:19:05 +1000

    adding treesitter selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,29 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+
 		dependancies = { "nvim-neorg/tree-sitter-norg-meta" },
+
 		build = ":TSUpdate",
 		event = {"VeryLazy" },
+
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
+
 				ensure_installed = { "lua", "norg", "c", "query", "markdown", "python" },
 				highlight = { enable = true },
 				indent = { enable = true },
 				auto_install = true,
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "gnn", -- set to `false` to disable one of the mappings
+						node_incremental = "grn",
+						scope_incremental = "grc",
+						node_decremental = "grm",
+					},
+				},
 			})
 		end,
 	},
