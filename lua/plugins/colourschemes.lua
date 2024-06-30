bc4767cddd4c9e381a44e016a28e3b845d428ee9
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-06-30 12:10:00 +1000

    just kanagawa

--- lua/plugins/colourschemes.lua
+++ lua/plugins/colourschemes.lua
@@ -10,11 +10,4 @@ return {
 			vim.cmd.colorscheme("kanagawa")
 		end,
 	},
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
 }
