b14721bc0bbdacf12daf7ce1755d808977c464b1
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-07-06 19:19:01 +1000

    stutusline
    i think startup is better than nougat

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,14 +1,21 @@
 return {
 	{
+		"echasnovski/mini.statusline",
+		version = "*",
+		config = function()
+			require("mini.statusline").setup()
+		end,
+	},
+	{
 		"echasnovski/mini.nvim",
 		-- event = "VeryLazy",
 		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 		version = "*",
 		config = function()
-			require("mini.ai").setup({})
+			require("mini.ai").setup()
 			-- require("mini.notify").setup({})
-			require("mini.pairs").setup({})
-			require("mini.splitjoin").setup({})
+			require("mini.pairs").setup()
+			require("mini.splitjoin").setup()
 
 			require("mini.move").setup({
 				mappings = {
@@ -23,6 +30,19 @@ return {
 					line_right = "L",
 					line_down = "J",
 					line_up = "K",
+				},
+			})
+			local hipatterns = require("mini.hipatterns")
+			hipatterns.setup({
+				highlighters = {
+					-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+					-- Highlight hex color strings (`#rrggbb`) using that color
+					hex_color = hipatterns.gen_highlighter.hex_color(),
 				},
 			})
 		end,
