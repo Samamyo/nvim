77bef6b196cf5873a8156b4fc41a51e6de863a8b
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-07-12 12:06:31 +1000

    adding undotree

---
+++ lua/plugins/undo.lua
@@ -0,0 +1,11 @@
+return {
+    "mbbill/undotree",
+    keys = {
+        {
+            "<leader>u",
+            ":UndotreeToggle<CR>:UndotreeFocus<CR>",
+            desc = "toggle undotree",
+            silent = true,
+        },
+    },
+}
