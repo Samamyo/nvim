ed51a51cf3d5c7687be95b34ba48386506fb3f2b
samamyo <157856367+Samamyo@users.noreply.github.com>
2024-07-12 12:09:30 +1000

    adding futher keymaps to harpoon
    adding <leader><number> 1-5

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,6 +7,11 @@ return {
         { "<C-w>" },
         { "<C-e>" },
         { "<C-r>" },
+        { "<leader>1" },
+        { "<leader>2" },
+        { "<leader>3" },
+        { "<leader>4" },
+        { "<leader>5" },
     },
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -24,6 +29,12 @@ return {
         vim.keymap.set("n", "<C-w>", function() harpoon:list():select(2) end, { nowait = true })
         vim.keymap.set("n", "<C-e>", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<C-r>", function() harpoon:list():select(4) end)
+
+        for _, idx in ipairs { 1, 2, 3, 4, 5 } do
+            vim.keymap.set("n", string.format("<space>%d", idx), function()
+                harpoon:list():select(idx)
+            end)
+        end
 
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<C-P>", function() harpoon:list():prev() end)
